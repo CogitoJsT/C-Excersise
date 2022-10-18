e3d2bc6cbdde86f7c0271b193902c71858224ddb
astrophenomenon <107760709+CogitoJsT@users.noreply.github.com>
2022-10-18 23:21:38 +0900

    Move inheriance.md file
    Create directory and move md file.
    Add inheritance example source code.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Book
+{
+    decimal isbn13;
+
+    public Book()
+    {
+        Console.WriteLine("Book default constructor");
+    }
+    public Book(decimal isbn13)
+    {
+        this.isbn13 = isbn13;
+        Console.WriteLine("Book constructor with isbn13");
+    }
+}
+
+class EBook : Book
+{
+    public EBook()
+    {
+        Console.WriteLine("EBook constructor");
+    }
+
+    public EBook(decimal isbn13)
+        : base(isbn13)
+    {
+        Console.WriteLine("EBook constructor with isbn13");
+    }
+}
+
+namespace Ineritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ebook = new EBook();
+            var ebook2 = new EBook(1234);
+        }
+    }
+}
